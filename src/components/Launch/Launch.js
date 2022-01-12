6a8b54a9687f22620b299f6dec7aa24acb285101
kniazivna <olenkaknyaz@gmail.com>
2022-01-12 13:34:17 +0200

    add css

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -5,10 +5,10 @@ import "./Launch.css"
 const Launch = (props) => {
     const {mission_name, launch_year, photo} = props;
     return (
-        <div>
+        <div className={'launch'}>
             <div className={'info'}><h1>Mission name: {mission_name}</h1>
                 <h2>Launch year: {launch_year}</h2></div>
-            <div>
+            <div className={'img'}>
                 <img className={'photo'} src={photo} alt={mission_name}/>
             </div>
         </div>
