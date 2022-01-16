a73e471ef9b8002f50084f94a77b4c1e655ec09c
kniazivna <olenkaknyaz@gmail.com>
2022-01-16 18:10:47 +0200

    classwork-2

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,42 +1,17 @@
-// import React, {useState} from 'react';
-//
-// const Form = ({getFiltered}) => {
-//
-//     const [form, setForm] = useState({name: '', username: '', email: ''});
-//
-//     const formHandler = (event) => {
-//         const info = {...form, [event.target.name]: event.target.value}
-//         setForm({...info});
-//     }
-//
-//     const find = (event) => {
-//         event.preventDefault();
-//         getFiltered(form);
-//     }
-//
-//     return (
-//         <form>
-//             <label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label>
-//             <label>Username: <input type="text" name={'username'} value={form.username} onChange={formHandler}/></label>
-//             <label>Email: <input type="text" name={'email'} value={form.age} onChange={formHandler}/></label>
-//             <button onClick={find}>Find</button>
-//         </form>
-//     );
-// };
-//
-// export default Form;
-
-
 import React, {useState} from 'react';
 
 const Form = ({getFiltered}) => {
 
+    const [form, setForm] = useState({name: '', username: '', email: ''});
 
-
+    const formHandler = (event) => {
+        const info = {...form, [event.target.name]: event.target.value}
+        setForm({...info});
+    }
 
     const find = (event) => {
         event.preventDefault();
-        getFiltered();
+        getFiltered(form);
     }
 
     return (
@@ -50,5 +25,7 @@ const Form = ({getFiltered}) => {
 };
 
 export default Form;
+
+
 
 
