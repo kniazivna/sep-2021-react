8010844b607b2c3348f301bdb79b772bf58caf71
kniazivna <olenkaknyaz@gmail.com>
2022-01-19 22:44:48 +0200

    homework-3

--- src/services/albums.service.js
+++
@@ -1,6 +0,0 @@
-import {axiosService} from "./axios.service";
-import {urls} from "../configs/urls";
-
-export const albumsService = {
-    getByUserId: (id) => axiosService.get(`${urls.albums}?userId=${id}`).then(value => value.data)
-}
