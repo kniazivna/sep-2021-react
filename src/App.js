a14f5c23df8cf40c21e4e3516234d7bc758d77e6
kniazivna <olenkaknyaz@gmail.com>
2022-01-27 16:42:34 +0200

    homework-4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import {useReducer} from "react";
+
 import './App.css';
+import {useForm} from "react-hook-form";
+
+const reducer = (state, action) => {
+  switch (action.type){
+    case 'add':
+      return{...state, cat: state.cat}
+    case 'delete':
+      return{...state, cat: state.cat}
+    default:
+      throw new Error('ERROR!!!')
+  }
+}
 
 function App() {
+  const [state,dispatch] = useReducer(reducer, {cat:0,dog:0} );
+
+  const {handleSubmit,register} = useForm();
+
+  const add = () =>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <form>
+            <label>Add cat: <input type="text" defaultValue={''}{...register('cat')}/></label>
+            <button onClick={handleSubmit(add)}>Save</button>
+            <label>Add dog: <input type="text" defaultValue={''}{...register('dog')}/></label>
+            <button onClick={handleSubmit(add)}>Save</button>
+        </form>
+    </>
   );
 }
 
