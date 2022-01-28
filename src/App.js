28bbced069697e083cde6596bbb1a56cad2f4e32
kniazivna <olenkaknyaz@gmail.com>
2022-01-28 19:52:06 +0200

    classwork-4

--- src/App.js
+++ src/App.js
@@ -1,37 +1,88 @@
-import {useReducer} from "react";
+import {useReducer, useRef} from "react";
 
 import './App.css';
-import {useForm} from "react-hook-form";
 
 const reducer = (state, action) => {
-  switch (action.type){
-    case 'add':
-      return{...state, cat: state.cat}
-    case 'delete':
-      return{...state, cat: state.cat}
-    default:
-      throw new Error('ERROR!!!')
-  }
+
+    switch (action.type) {
+
+        case 'add':
+            if (action.target === 'cat') {
+                return {...state, cat: [...state.cat, action.data]}
+            } else if (action.target === 'dog') {
+                return {...state, dog: [...state.dog, action.data]}
+            }
+            break;
+
+        case 'delete':
+            if (action.target === 'dog') {
+                return {...state, dog: [...state.dog, action.data]}
+            }
+    }
 }
 
 function App() {
-  const [state,dispatch] = useReducer(reducer, {cat:0,dog:0} );
 
-  const {handleSubmit,register} = useForm();
+    const [state, dispatch] = useReducer(reducer, {cat: [], dog: []});
 
-  const add = () =>{
+    const catRef = useRef();
+    const dogRef = useRef();
 
-  }
-  return (
-    <>
-        <form>
-            <label>Add cat: <input type="text" defaultValue={''}{...register('cat')}/></label>
-            <button onClick={handleSubmit(add)}>Save</button>
-            <label>Add dog: <input type="text" defaultValue={''}{...register('dog')}/></label>
-            <button onClick={handleSubmit(add)}>Save</button>
-        </form>
-    </>
-  );
+    const catCreator = (e) => {
+        e.preventDefault();
+        const cat = catRef.current.value;
+        dispatch({type: 'add', target: 'cat', data: cat})
+    }
+
+    const dogCreator = (e) => {
+        e.preventDefault();
+        const dog = dogRef.current.value;
+        dispatch({type: 'add', target: 'dog', data: dog})
+    }
+
+    // const catRemover = (e) => {
+    //     e.preventDefault();
+    //     const cat = document.getElementById().innerHTML = '';
+    //     // cat.classList.add('hidden');
+    //     dispatch({type: 'delete', target: 'cat', data: cat})
+    // }
+
+    const dogRemover = (e) => {
+        e.preventDefault();
+        const dog = document.getElementById().classList.toggle('hidden');
+
+        // dog.classList.toggle('hidden');
+        dispatch({type: 'delete', target: 'dog', data: dog})
+    }
+
+    return (
+        <>
+            <form>
+                <label>Add cat: <input type="text" name={'cat'} ref={catRef}/></label>
+                <button onClick={catCreator}>Save</button>
+                <label>Add dog: <input type="text" name={'dog'} ref={dogRef}/></label>
+                <button onClick={dogCreator}>Save</button>
+            </form>
+
+            <div className={'block'}>
+                <div className={'animals'}>
+                    {/*{(state.cat).map(value => <div className={'cat'} key={(state.cat).indexOf(value)}*/}
+                    {/*                               id={(state.cat).indexOf(value)}>*/}
+                    {/*    <p>{value}</p>*/}
+                    {/*    <button>Delete</button>*/}
+                    {/*</div>)}*/}
+                </div>
+                <div className={'animals'}>
+                    {(state.dog).map(value => <div className={'dog'} key={(state.dog).indexOf(value)}
+                                                   id={(state.dog).indexOf(value)}>
+                        <p>{value}</p>
+                        <button onClick={dogRemover}>Delete</button>
+                    </div>)}
+                </div>
+            </div>
+        </>
+    );
+
 }
 
 export default App;
